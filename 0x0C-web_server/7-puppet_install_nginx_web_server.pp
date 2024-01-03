a9bb9b23443e1640bce5e761cea9065cc08b3664
Youssef-Mafa <youssefmafanze@gmail.com>
2024-01-03 08:20:35 -0800

    a

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,6 +9,30 @@ service{ 'nginx':
   enable => 'true',
 }
 
+file{'/etc/nginx/sites-available/default':
+  ensure => 'file',
+  content => 'server {
+    listen 80 default_server;
+    listen [::]:80 default_server ipv6only=on;
+
+    root /var/www/html;
+    index index.html index.htm;
+
+    server_name localhost;
+    error_page 404 /404.html;
+    location / {
+        try_files $uri $uri/ =404;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}',
+  require => Package['nginx'],
+  notify => Service['nginx'],
+  replace => 'true',
+}
+
 file {'/var/www/html/index.html':
   ensure => 'file',
   content => 'Hello World!',
